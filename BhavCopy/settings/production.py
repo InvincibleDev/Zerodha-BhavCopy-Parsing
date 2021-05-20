4bf65659209cd29bc92072bc19590b116a90f312
root <root@ip-172-31-40-51.ap-south-1.compute.internal>
2021-05-20 11:13:57 +0000

    Updated allowed host and CORS

--- BhavCopy/settings/production.py
+++ BhavCopy/settings/production.py
@@ -4,10 +4,11 @@ SECRET_KEY = 'django-insecure-%+^q4t^a3$+w*&li^l3q_-)nv58(#j2#_rdc4bcdd7$0_*y)_m
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["http://zerodha-bhav-copy.s3-website.ap-south-1.amazonaws.com",
+"ec2-35-154-206-196.ap-south-1.compute.amazonaws.com"]
 
 CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = ["*"]
+CORS_ORIGIN_WHITELIST = ["http://zerodha-bhav-copy.s3-website.ap-south-1.amazonaws.com"]
 
 print("\n" + "-" * 60)
 print("Running server with production settings..")
